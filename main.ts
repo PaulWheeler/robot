8f00d658b45f36fcc17f43e8dd5e93a454833c69
Paul Stuart Wheeler <paul.stuart.wheeler@gmail.com>
2023-01-11 06:51:32 +0000

    A+B draws a star

--- main.ts
+++ main.ts
@@ -28,6 +28,25 @@ function drawPentagon () {
 input.onButtonPressed(Button.A, function () {
     drawSquare()
 })
+input.onButtonPressed(Button.AB, function () {
+    drawStar()
+})
 input.onButtonPressed(Button.B, function () {
     drawPentagon()
 })
+function drawStar () {
+    Kitronik_Move_Motor.beepHorn()
+    Kitronik_Move_Motor.beepHorn()
+    Kitronik_Move_Motor.beepHorn()
+    for (let index = 0; index < 5; index++) {
+        Kitronik_Move_Motor.turnRadius(Kitronik_Move_Motor.TurnRadii.Tight)
+        Kitronik_Move_Motor.move(Kitronik_Move_Motor.DriveDirections.Forward, 20)
+        basic.pause(3000)
+        Kitronik_Move_Motor.stop()
+        Kitronik_Move_Motor.spin(Kitronik_Move_Motor.SpinDirections.Right, 15)
+        basic.pause(2360)
+        Kitronik_Move_Motor.stop()
+    }
+    Kitronik_Move_Motor.beepHorn()
+    Kitronik_Move_Motor.beepHorn()
+}
