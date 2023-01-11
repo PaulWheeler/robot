f866d812af2274aab7ae81b4afda08b758db7164
Paul Stuart Wheeler <paul.stuart.wheeler@gmail.com>
2023-01-11 06:45:23 +0000

    Press A to draw a more accurate square. Press B for a pentagon

--- main.ts
+++ main.ts
@@ -1,22 +1,33 @@
 function drawSquare () {
+    Kitronik_Move_Motor.beepHorn()
+    Kitronik_Move_Motor.beepHorn()
     for (let index = 0; index < 4; index++) {
         Kitronik_Move_Motor.move(Kitronik_Move_Motor.DriveDirections.Forward, 20)
-        basic.pause(1000)
+        basic.pause(2000)
         Kitronik_Move_Motor.stop()
         Kitronik_Move_Motor.spin(Kitronik_Move_Motor.SpinDirections.Right, 15)
-        basic.pause(1300)
+        basic.pause(1475)
         Kitronik_Move_Motor.stop()
     }
+    Kitronik_Move_Motor.beepHorn()
+    Kitronik_Move_Motor.beepHorn()
 }
 function drawPentagon () {
+    Kitronik_Move_Motor.beepHorn()
     for (let index = 0; index < 5; index++) {
         Kitronik_Move_Motor.turnRadius(Kitronik_Move_Motor.TurnRadii.Tight)
         Kitronik_Move_Motor.move(Kitronik_Move_Motor.DriveDirections.Forward, 20)
-        basic.pause(1000)
+        basic.pause(2000)
         Kitronik_Move_Motor.stop()
         Kitronik_Move_Motor.spin(Kitronik_Move_Motor.SpinDirections.Right, 15)
-        basic.pause(1000)
+        basic.pause(1180)
         Kitronik_Move_Motor.stop()
     }
+    Kitronik_Move_Motor.beepHorn()
 }
-drawSquare()
+input.onButtonPressed(Button.A, function () {
+    drawSquare()
+})
+input.onButtonPressed(Button.B, function () {
+    drawPentagon()
+})
